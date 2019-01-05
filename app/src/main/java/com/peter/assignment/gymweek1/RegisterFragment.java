89c1ad2a5031c0270696148dc89ace529d9d203b
petercin777 <704790665@qq.com>
2019-01-06 04:41:32 +0800

    Initial commit

---
+++ app/src/main/java/com/peter/assignment/gymweek1/RegisterFragment.java
@@ -0,0 +1,18 @@
+package com.peter.assignment.gymweek1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class RegisterFragment extends Fragment {
+    private Button mSubButton;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.register, container, false);
+    }
+}
